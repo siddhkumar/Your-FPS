61a2c5bc3d805d3673f48a93eb5f2c133d3bbe48
Siddharth Kumar <siddharth963@outlook.com>
2021-12-29 22:18:19 +0530

    Improved FootstepAudioPlayer for a smoother transition between playing and stopped state

--- Elements/FootstepAudioPlayer/FootstepAudioPlayer.gd
+++ Elements/FootstepAudioPlayer/FootstepAudioPlayer.gd
@@ -2,14 +2,19 @@ extends Spatial
 
 onready var audio_player = $AudioStreamPlayer
 
-var counting = false
-var count = 0
+var counting_to_stop = false
+var counting_to_start = true
+var count_stop = 0
+var count_start = 0
 var stopped_at = 0
 
-const time_to_stop_audio = 0.3
+const time_to_stop_audio = 0.25
+const time_to_start_audio = 0.25
 
 func play():
-	audio_player.play(stopped_at)
+	counting_to_stop = false
+	audio_player.volume_db = 0
+	counting_to_start = true
 	stopped_at = 0
 	pass
 
@@ -18,17 +23,30 @@ func is_playing():
 	pass
 
 func stop():
-	counting = true
+	counting_to_start = false
+	counting_to_stop = true
 	pass
 
-func _process(delta):
-	if counting:
-		count += delta 
-		audio_player.volume_db -= 0.5
+func _physics_process(delta):
+	print(audio_player.volume_db)
+	if counting_to_start:
+		audio_player.play(stopped_at)
+		count_start += delta
+		audio_player.volume_db += 0.0333
 	
-	if count > time_to_stop_audio:
+	if count_start > time_to_start_audio:
+		counting_to_start = false
+		count_start = 0
+	
+	
+	
+	if counting_to_stop:
+		count_stop += delta 
+		audio_player.volume_db -= 0.0333
+	
+	if count_stop > time_to_stop_audio:
 		stopped_at = audio_player.get_playback_position()
 		audio_player.volume_db = 0
-		count = 0
-		counting = false 
+		count_stop = 0
+		counting_to_stop = false 
 		audio_player.stop()
