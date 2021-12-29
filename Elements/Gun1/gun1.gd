7953bda618018f5ba5b1b0cff748752bb2f1e668
Siddharth Kumar <siddharth963@outlook.com>
2021-12-29 14:31:43 +0530

    Implemented Muzzle Flash as a child to the gun1 node, implemented script for the same

--- Elements/Gun1/gun1.gd
+++ Elements/Gun1/gun1.gd
@@ -1,9 +1,15 @@
 extends Spatial
 
+var time = 0
+var time_between_bullets = 0.2
+
 func _process(delta):
-	if Input.is_action_pressed("shoot"):
+	time += delta
+	if Input.is_action_pressed("shoot") and time > time_between_bullets:
 		shoot()
-	else:
+		time = 0
+	
+	if !Input.is_action_pressed("shoot"):
 		if $AnimationPlayer.current_animation_position < 0.06 :
 			$AnimationPlayer.stop()
 
@@ -11,6 +17,10 @@ func _process(delta):
 
 func shoot():
 	play_animation()
+	muzzle_flash()
 
 func play_animation():
 	$AnimationPlayer.play("Cube002Action")
+
+func muzzle_flash():
+	$Cube002/MuzzleFlash.emitting = true
